09545614e71d1daf24248aff47bbdbec885e9c37
bluearth <barkah.yusuf@gmail.com>
2014-10-21 23:17:26 +0700

    Capdev

--- orca-app/src/main/java/com/akiradata/orca/capture/CaptureDeviceException.java
+++ orca-app/src/main/java/com/akiradata/orca/capture/CaptureDeviceException.java
@@ -4,4 +4,30 @@ public class CaptureDeviceException extends Exception {
 
 	private static final long serialVersionUID = -2899282425056203978L;
 
+	public CaptureDeviceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CaptureDeviceException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CaptureDeviceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CaptureDeviceException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CaptureDeviceException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- orca-app/src/main/java/com/akiradata/orca/capture/directory/DirectoryCaptureDevice.java
+++ orca-app/src/main/java/com/akiradata/orca/capture/directory/DirectoryCaptureDevice.java
@@ -1,7 +1,12 @@
 package com.akiradata.orca.capture.directory;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import javax.imageio.IIOImage;
 
 import javafx.scene.Node;
 
@@ -10,32 +15,52 @@ import org.controlsfx.dialog.Dialog;
 import com.akiradata.orca.capture.CaptureDevice;
 import com.akiradata.orca.capture.CaptureDeviceEvent;
 import com.akiradata.orca.capture.CaptureDeviceEvent.Type;
+import com.akiradata.orca.capture.CaptureDeviceException;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
 
 public class DirectoryCaptureDevice extends CaptureDevice {
 
 	File rootDirectory;
 	List<String> imageFormat;
-
-
+	List<IIOImage> imageBuffer = new ArrayList<IIOImage>(10);
+	
 	@Override
-	public void capture() {
-		fireCaptureDeviceEvent(new CaptureDeviceEvent(this, Type.CONFIG_REQUESTED));
+	public void capture() throws CaptureDeviceException {
+		
+		fireCaptureDeviceEvent(new CaptureDeviceEvent(this, Type.CAPTURE_STARTED));
+		
+		if (!rootDirectory.exists()) throw new CaptureDeviceException("Path selected does not exists");
+		if (!rootDirectory.isDirectory()) throw new CaptureDeviceException("Path selected is not a directory");
+		if (!rootDirectory.canRead()) throw new CaptureDeviceException("Could not read the path selected");
+		
+		List<File> files = Arrays.asList(
+				rootDirectory.listFiles((file) -> 
+					imageFormat.stream().anyMatch(file.getName()::endsWith)));
+		
+		
+		for (File imgFile : files) {
+			fireCaptureDeviceEvent(new CaptureDeviceEvent(this, Type.PAGE_STARTED));
+			
+			
+			fireCaptureDeviceEvent(new CaptureDeviceEvent(this, Type.PAGE_COMPLETED));
+		}
+		
+		fireCaptureDeviceEvent(new CaptureDeviceEvent(this, Type.CAPTURE_COMPLETED));
+		
 	}
 
 	@Override
 	public void configure() {
-		
+		fireCaptureDeviceEvent(new CaptureDeviceEvent(this, Type.CONFIG_REQUESTED));
 	}
 
 	@Override
 	public void release() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public String getName() {
-		return "Directory";
+		return "Directory capture device";
 	}
 
 	@Override
